0d3864092d2dcc5b89099ba35cf6cdbac0476075
Theo Booth <theorlbooth@googlemail.com>
2020-11-12 12:38:17 +0000

    pubs cards page started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import '../styles/style.scss'
 
 import Home from './components/Home'
 import NavBar from './components/Navbar'
+import PubList from './components/PubList'
 
 const App = () => {
   return <>
@@ -15,9 +16,9 @@ const App = () => {
     <Switch>
       <Route exact path='/' component={Home} />
       {/* <Route exact path='/signup' component={Signup} />
-      <Route exact path='login' component={Login} />
+      <Route exact path='login' component={Login} /> */}
       <Route exact path='/pubs' component={PubList} />
-      <Route exact path='/pubs/:pubId' component={SinglePub} />
+      {/* <Route exact path='/pubs/:pubId' component={SinglePub} />
       <Route exact path='/pubs/new-pub' component={CreatePub} />
       <Route exact path='/pubs/edit-pub/:pubId' component={EditPub} /> */}
     </Switch>
