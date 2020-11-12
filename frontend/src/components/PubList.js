cea439bc9731d6af3e9d139a69c5f31d800ba4e3
Theo Booth <theorlbooth@googlemail.com>
2020-11-12 16:42:47 +0000

    Bigger Seed

--- frontend/src/components/PubList.js
+++ frontend/src/components/PubList.js
@@ -17,6 +17,29 @@ const PubList = () => {
   const [liveSportTog, updateLiveSportTog] = useState(false)
 
 
+  function checkForImage(pub) {
+    if (pub.imageUrl === '') {
+      return 'https://images.unsplash.com/photo-1586993451228-09818021e309?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
+    }
+    return pub.imageUrl
+  }
+
+  // ! Function to check when user changes location but not to send until Enter is hit or button is pressed 
+  // ! Props will need to be passed through from home page
+  // function checkEnter(event) {
+  //   const key = event.keyCode
+  //   if (key === 13) {
+  //     if (event.target.vaue === '') {
+  //       props.updateLocation()
+  //     } else {
+  //       props.updateLocation(event.taget.value)
+  //     }
+  //   }
+  // }
+  // ! Add to input:
+  // onKeyDown={event => checkEnter(event)}
+
+
   function filterPubs(pubsList) {
     let filteredPubsA
     let filteredPubsB
@@ -33,7 +56,6 @@ const PubList = () => {
         return pub.takeAway === true
       })
     }
-    console.log('1')
     if (filters[1] === false) {
       filteredPubsB = filteredPubsA
     } else {
@@ -41,7 +63,6 @@ const PubList = () => {
         return pub.outdoorSeating === true
       })
     }
-    console.log('2')
     if (filters[2] === false) {
       filteredPubsC = filteredPubsB
     } else {
@@ -49,7 +70,6 @@ const PubList = () => {
         return pub.heating === true
       })
     }
-    console.log('3')
     if (filters[3] === false) {
       filteredPubsD = filteredPubsC
     } else {
@@ -57,7 +77,6 @@ const PubList = () => {
         return pub.liveMusic === true
       })
     }
-    console.log('4')
     if (filters[4] === false) {
       filteredPubsE = filteredPubsD
     } else {
@@ -65,11 +84,10 @@ const PubList = () => {
         return pub.liveSport === true
       })
     }
-    console.log('5')
     return filteredPubsE
   }
 
-  console.log(filterPubs(examplePubs))
+  //  !! console.log(filterPubs(examplePubs))
   //  !! Remember to change back below!!
 
   useEffect(() => {
@@ -86,58 +104,60 @@ const PubList = () => {
   }
 
   return <>
-    <div className="filter">
-      <div className="search">
-        <input placeholder="user-input-coordinates"></input>
-        <button>Update Coordinates</button>
+    <div className="pubs-page">
+      <div className="filter">
+        <div className="search">
+          <input placeholder="user-input-coordinates"></input>
+          <button>New Location</button>
+        </div>
+        <div className="toggles">
+          <div>
+            <Toggle id="take-away-toggle" className="react-toggle" defaultChecked={false} onChange={(event) => updateTakeAwayTog(event.target.checked)} />
+            <label htmlFor="take-away-toggle">Take Away</label>
+          </div>
+          <div>
+            <Toggle id="outdoor-seating-toggle" className="react-toggle" defaultChecked={false} onChange={(event) => updateOutdoorSeatingTog(event.target.checked)} />
+            <label htmlFor="outdoor-seating-toggle">Outdoor Seating</label>
+          </div>
+          <div>
+            <Toggle id="heating-toggle" className="react-toggle" defaultChecked={false} onChange={(event) => updateHeatingTog(event.target.checked)} />
+            <label htmlFor="heating-toggle">Heating</label>
+          </div>
+          <div>
+            <Toggle id="live-music-toggle" className="react-toggle" defaultChecked={false} onChange={(event) => updateLiveMusicTog(event.target.checked)} />
+            <label htmlFor="live-music-toggle">Live Music</label>
+          </div>
+          <div>
+            <Toggle id="live-sport-toggle" className="react-toggle" defaultChecked={false} onChange={(event) => updateLiveSportTog(event.target.checked)} />
+            <label htmlFor="live-sport-toggle">Live Sport</label>
+          </div>
+        </div>
       </div>
-      <div className="toggles">
-        <div>
-          <Toggle id="take-away-toggle" className="react-toggle" defaultChecked={false} onChange={(event) => updateTakeAwayTog(event.target.checked)} />
-          <label htmlFor="take-away-toggle">Take Away</label>
-        </div>
-        <div>
-          <Toggle id="outdoor-seating-toggle" className="react-toggle" defaultChecked={false} onChange={(event) => updateOutdoorSeatingTog(event.target.checked)} />
-          <label htmlFor="outdoor-seating-toggle">Outdoor Seating</label>
-        </div>
-        <div>
-          <Toggle id="heating-toggle" className="react-toggle" defaultChecked={false} onChange={(event) => updateHeatingTog(event.target.checked)} />
-          <label htmlFor="heating-toggle">Heating</label>
-        </div>
-        <div>
-          <Toggle id="live-music-toggle" className="react-toggle" defaultChecked={false} onChange={(event) => updateLiveMusicTog(event.target.checked)} />
-          <label htmlFor="live-music-toggle">Live Music</label>
-        </div>
-        <div>
-          <Toggle id="live-sport-toggle" className="react-toggle" defaultChecked={false} onChange={(event) => updateLiveSportTog(event.target.checked)} />
-          <label htmlFor="live-sport-toggle">Live Sport</label>
-        </div>
-      </div>
-    </div>
 
-    {/* !! Change back to pubsList !! */}
-    <div className="search-results">
-      <div className="columns is-multiline is-mobile">
-        {filterPubs(examplePubs).map((pub, index) => {
-          return <div className="column is-2-desktop is-6-tablet is-12-mobile" key={index}>
-            <Link to={`pub/${pub._id}`}>
-              <div className="card">
-                <div className="card-image">
-                  <figure className="image is-square">
-                    <img src={pub.imageUrl} alt={pub.name} />
-                  </figure>
-                </div>
-                <div className="card-content">
-                  <div className="media-content">
-                    <h2 className="title is-5">{pub.name}</h2>
-                    <p className="subtitle is-6">{pub.address.address1}, {pub.address.zip_code}</p>
-                    <p className="subtitle is-6">Distance from:</p>
+      {/* !! Change back to pubsList !! */}
+      <div className="search-results">
+        <div className="columns is-multiline is-mobile">
+          {filterPubs(pubsList).map((pub, index) => {
+            return <div className="column is-2-desktop is-6-tablet is-12-mobile" key={index}>
+              <Link to={`pub/${pub._id}`}>
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="image is-square">
+                      <img src={checkForImage(pub)} alt={pub.name} />
+                    </figure>
+                  </div>
+                  <div className="card-content">
+                    <div className="media-content">
+                      <h2 className="title is-5">{pub.name}</h2>
+                      <p className="subtitle is-6">{pub.address.address1}, {pub.address.zip_code}</p>
+                      <p className="subtitle is-6">Distance from:</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-          </div>
-        })}
+              </Link>
+            </div>
+          })}
+        </div>
       </div>
     </div>
   </>
