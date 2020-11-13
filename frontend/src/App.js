c388a743417bece267c65262155a04aab1a28cb5
Adam Osgood <adamlee.osgood@gmail.com>
2020-11-13 11:15:14 +0100

    day3 start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,14 +7,11 @@ import '../styles/style.scss'
 
 import Home from './components/Home'
 import NavBar from './components/Navbar'
-<<<<<<< HEAD
 import Signup from './components/Signup'
 import Login from './components/Login'
 import PubList from './components/PubList'
 import CreatePub from './components/CreatePub'
-=======
 import Maps from './components/Maps'
->>>>>>> Adambranch
 
 const App = () => {
   return <>
@@ -22,20 +19,12 @@ const App = () => {
     <NavBar />
     <Switch>
       <Route exact path='/' component={Home} />
-<<<<<<< HEAD
       <Route exact path='/signup' component={Signup} />
       <Route exact path='/login' component={Login} />
-      <Route exact path='login' component={Login} /> */}
-=======
+      <Route exact path='/pubs' component={PubList} /> 
       <Route exact path='/pubs/maps' component={Maps}/>
-      {/* <Route exact path='/signup' component={Signup} />
-      <Route exact path='login' component={Login} />
-
->>>>>>> Adambranch
-      <Route exact path='/pubs' component={PubList} />
-      {/* <Route exact path='/pubs/:pubId' component={SinglePub} /> */}
       <Route exact path='/pubs/new-pub' component={CreatePub} />
-      {/* <Route exact path='/pubs/edit-pub/:pubId' component={EditPub} /> */}
+
     </Switch>
   </BrowserRouter>
   </>
