4548405468955981250f6e21afd9b584bbaa8c9c
Adam Osgood <adamlee.osgood@gmail.com>
2020-11-13 14:38:34 +0100

    Maps working-ish

--- frontend/src/components/Maps.js
+++ frontend/src/components/Maps.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef, useCallback } from 'react'
 import { Link, withRouter } from 'react-router-dom'
-import ReactMapGL, { Marker, Popup } from 'react-map-gl'
+import ReactMapGL, { Marker, Popup, GeolocateControl } from 'react-map-gl'
 import axios from 'axios'
+import Geocoder from "react-map-gl-geocoder";
 
 
 
@@ -15,20 +16,27 @@ const DisplayMap = () => {
     latitude: 51.5721642,
     longitude: -0.0083677,
     width: "100vw",
-    height: "100vh",
+    height: "90vh",
     zoom: 10
   })
 
-  const [viewportDefault, setviewportDefault] = useState({
-    latitude: userInput[0],
-    longitude: userInput[1],
-    width: "100vw",
-    height: "100vh",
-    zoom: 10,
-    radiusPin: []
-  })
 
+  const geolocateStyle = {
+    float: 'right',
+    margin: '50px',
+    padding: '10px'
+  };
 
+  // const [viewportDefault, setviewportDefault] = useState({
+  //   latitude: userInput[0],
+  //   longitude: userInput[1],
+  //   width: "100vw",
+  //   height: "100vh",
+  //   zoom: 10,
+  //   radiusPin: []
+  // })
+
+  // mapRef = React.createRef()
 
 
   useEffect(() => {
@@ -38,19 +46,44 @@ const DisplayMap = () => {
       })
   }, [])
 
+  const mapRef = useRef();
+  const handleViewportChange = useCallback(
+    (newViewport) => setViewport(newViewport),
+    []
+  );
 
+  const handleGeocoderViewportChange = useCallback(
+    (newViewport) => {
+      const geocoderDefaultOverrides = { transitionDuration: 1000 };
 
-
+      return handleViewportChange({
+        ...newViewport,
+        ...geocoderDefaultOverrides
+      });
+    },
+    []
+  );
 
   return <section>
     <ReactMapGL
+      ref={mapRef}
       {...viewport}
       mapStyle='mapbox://styles/adwam12/ckhewfl88137g19rzckkwjfv0'
+
       mapboxApiAccessToken="pk.eyJ1IjoiYWR3YW0xMiIsImEiOiJja2hlc3Rvbm8wNTd5MzBtMnh4d3I3ODR3In0.-MLW5F1IEhhA-2jgTww4_w"
-      onViewportChange={viewport => {
-        setViewport(viewport)
-      }}
+      onViewportChange={handleViewportChange}
     >
+      <Geocoder
+        mapRef={mapRef}
+        onViewportChange={handleGeocoderViewportChange}
+        mapboxApiAccessToken="pk.eyJ1IjoiYWR3YW0xMiIsImEiOiJja2hlc3Rvbm8wNTd5MzBtMnh4d3I3ODR3In0.-MLW5F1IEhhA-2jgTww4_w"
+        position="top-left"
+      />
+      <GeolocateControl
+        style={geolocateStyle}
+        positionOptions={{ enableHighAccuracy: true }}
+        trackUserLocation={true}
+      />
       {pubList.map((pub, index) => {
         {/* {console.log(pub)} */ }
         return <Marker latitude={pub.coordinates.latitude} longitude={pub.coordinates.longitude} key={index}>
@@ -62,11 +95,15 @@ const DisplayMap = () => {
           </button>
         </Marker>
       })}
+      <Marker latitude={0} longitude={0}>
+        <h1>HELLO</h1>
+      </Marker>
       {selectedPub ? (
-        <Popup latitude={selectedPub.coordinates.latitude} longitude={selectedPub.coordinates.longitude} onClose={() =>{setSelectedPub(null)}}>
+        <Popup latitude={selectedPub.coordinates.latitude} longitude={selectedPub.coordinates.longitude} onClose={() => { setSelectedPub(null) }}>
           <div>
             <h2>{selectedPub.name}</h2>
             <p>{selectedPub.address.address1}</p>
+            <button onClick={console.log("clicked")}>More...</button>
           </div>
         </Popup>
       ) : null}
@@ -89,3 +126,63 @@ const Maps = () => {
 
 
 export default Maps
+
+
+
+
+// import 'mapbox-gl/dist/mapbox-gl.css'
+// import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
+// import React, { useState, useRef, useCallback } from 'react'
+// import MapGL from 'react-map-gl'
+// import Geocoder from 'react-map-gl-geocoder'
+
+// // Ways to set Mapbox token: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
+// const MAPBOX_TOKEN = 'pk.eyJ1IjoiYWR3YW0xMiIsImEiOiJja2hlc3Rvbm8wNTd5MzBtMnh4d3I3ODR3In0.-MLW5F1IEhhA-2jgTww4_w'
+
+// const Example = () => {
+//   const [viewport, setViewport] = useState({
+//     latitude: 37.7577,
+//     longitude: -122.4376,
+//     zoom: 8
+//   });
+  // const mapRef = useRef();
+  // const handleViewportChange = useCallback(
+  //   (newViewport) => setViewport(newViewport),
+  //   []
+  // );
+
+//   // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  // const handleGeocoderViewportChange = useCallback(
+  //   (newViewport) => {
+  //     const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+  //     return handleViewportChange({
+  //       ...newViewport,
+  //       ...geocoderDefaultOverrides
+  //     });
+  //   },
+  //   []
+  // );
+
+//   return (
+//     <div style={{ height: "100vh" }}>
+//       <MapGL
+        // ref={mapRef}
+//         {...viewport}
+//         width="100%"
+//         height="100%"
+        // onViewportChange={handleViewportChange}
+//         mapboxApiAccessToken={MAPBOX_TOKEN}
+//       >
+        // <Geocoder
+        //   mapRef={mapRef}
+        //   onViewportChange={handleGeocoderViewportChange}
+        //   mapboxApiAccessToken={MAPBOX_TOKEN}
+        //   position="top-left"
+        // />
+//       </MapGL>
+//     </div>
+//   );
+// };
+
+// export default Example
