549271b82f257978dd45e652b629d652671fcf8c
Theo Booth <theorlbooth@googlemail.com>
2020-11-20 14:44:04 +0000

    still trying to deploy

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -84,25 +84,25 @@ const Home = () => {
   console.log(userStatus)
   function statusCheck() {
 
-    if (userStatus === 'false' ){
+    if (userStatus === 'false') {
       return <div>
-                        <Link to={`/users/${getUserId()}`}>
-      <p>Account</p>
-      <p style={{ fontSize: '25px', fontWeight: '400' }}>Please validate your email </p>
-      </Link>
-    </div>
-    }
-    if (userStatus === true ){
-      return <div>
-                <Link to={`/users/${getUserId()}`}>
-        <p>Account</p>
+        <Link to={`/users/${getUserId()}`}>
+          <p>Account</p>
+          <p style={{ fontSize: '25px', fontWeight: '400' }}>Please validate your email </p>
         </Link>
       </div>
     }
-    if (userStatus === 'none'){
+    if (userStatus === true) {
       return <div>
-        <Link to={`/login`}>
-        <p>Login/Register</p>
+        <Link to={`/users/${getUserId()}`}>
+          <p>Account</p>
+        </Link>
+      </div>
+    }
+    if (userStatus === 'none') {
+      return <div>
+        <Link to={'/login'}>
+          <p>Login/Register</p>
         </Link>
       </div>
     }
@@ -110,27 +110,27 @@ const Home = () => {
   }
 
   return <div
-    style={{ display: "flex", marginLeft: "25%", marginTop: '5%' }}
+    style={{ display: 'flex', marginLeft: '25%', marginTop: '5%' }}
   >
 
     <div className="tile is-ancestor">
       <div className="tile is-vertical is-8">
         <div className="tile">
           <div className="tile is-parent is-vertical">
-            <article className="tile is-child notification" style={{ display: 'flex', justifyContent: 'center', alignitems: 'center', border: '10px solid white', backgroundColor: "rgba(0,0,0,0.5)" }}>
-            <Link to={`/pubs`} style={{display: 'flex', alignSelf: 'center', color: "white"}}>
-              <h2 style={{  fontSize: '50px', fontWeight: '900', color: 'white' }}>PUB LIST</h2>
+            <article className="tile is-child notification" style={{ display: 'flex', justifyContent: 'center', alignitems: 'center', border: '10px solid white', backgroundColor: 'rgba(0,0,0,0.5)' }}>
+              <Link to={'/pubs'} style={{ display: 'flex', alignSelf: 'center', color: 'white' }}>
+                <h2 style={{ fontSize: '50px', fontWeight: '900', color: 'white' }}>PUB LIST</h2>
               </Link>
             </article>
-            <article className="tile is-child notification is-dark" style={{ display: 'flex', justifyContent: 'center', alignitems: 'center', border: '10px solid white', backgroundColor: "rgba(0,0,0,0.5)" }}>
+            <article className="tile is-child notification is-dark" style={{ display: 'flex', justifyContent: 'center', alignitems: 'center', border: '10px solid white', backgroundColor: 'rgba(0,0,0,0.5)' }}>
               <p style={{ display: 'flex', alignSelf: 'center', fontSize: '50px', fontWeight: '900', color: 'white' }}>{statusCheck()}</p>
               {/* <p className="subtitle">Bottom tile</p> */}
             </article>
           </div>
           <div className="tile is-parent">
             <article className="tile is-child notification" style={{ border: '10px solid white', backgroundColor: 'rgba(0,0,0,0.5)' }}>
-              <Link to={`/pubs/maps`} style={{color: 'white'}}>
-              <p style={{ display: "flex", alignSelf: 'center', fontSize: '50px', fontWeight: '900', color: 'white' }}>Map</p>
+              <Link to={'/pubs/maps'} style={{ color: 'white' }}>
+                <p style={{ display: 'flex', alignSelf: 'center', fontSize: '50px', fontWeight: '900', color: 'white' }}>Map</p>
               </Link>
               <ReactMapGL
                 ref={mapRef}
@@ -145,8 +145,8 @@ const Home = () => {
           </div>
         </div>
         {id && isVerified(user) ? <div className="tile is-parent">
-          <article className="tile is-child notification" style={{ display: 'flex', justifyContent: "center", alignitems: "center", border: '10px solid white', backgroundColor: "rgba(0,0,0,0.5)", flexDirection:'column' }}>
-            <p className="title" style={{ display: "flex", alignSelf: 'center', fontSize: '50px', fontWeight: '900', color: 'white' }}>User Search</p>
+          <article className="tile is-child notification" style={{ display: 'flex', justifyContent: 'center', alignitems: 'center', border: '10px solid white', backgroundColor: 'rgba(0,0,0,0.5)', flexDirection: 'column' }}>
+            <p className="title" style={{ display: 'flex', alignSelf: 'center', fontSize: '50px', fontWeight: '900', color: 'white' }}>User Search</p>
             <input
               className="input"
               onChange={(event) => {
@@ -156,7 +156,7 @@ const Home = () => {
             ></input>
             {filterText ? <div>
               {filterUsers().map((user, index) => {
-                return <div key={index} className="tile is-child notification is-dark" style={{ display: 'flex', justifyContent: "center", alignitems: "center", border: '5px solid white', backgroundColor: "rgba(0,0,0,0.5)", margin: '5px' }}>
+                return <div key={index} className="tile is-child notification is-dark" style={{ display: 'flex', justifyContent: 'center', alignitems: 'center', border: '5px solid white', backgroundColor: 'rgba(0,0,0,0.5)', margin: '5px' }}>
                   <h3 className='pr-2'>{user.username}</h3>
                   <Link className='is-button' to={`/users/${user._id}`}>View Profile</Link>
                 </div>
